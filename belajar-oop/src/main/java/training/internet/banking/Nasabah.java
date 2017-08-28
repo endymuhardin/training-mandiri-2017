d360c76d5b1511cf8729a1fa7a21a5f90c1fd5eb
Endy Muhardin <endy.muhardin@gmail.com>
2017-08-28 11:25:05 +0700

    konversi jadi maven project

---
+++ belajar-oop/src/main/java/training/internet/banking/Nasabah.java
@@ -0,0 +1,13 @@
+package training.internet.banking;
+
+public class Nasabah {
+	private String nama;
+
+	public Nasabah(String nama) {
+		this.nama = nama;
+	}
+
+	public String getNama() {
+		return this.nama;
+	}
+}
