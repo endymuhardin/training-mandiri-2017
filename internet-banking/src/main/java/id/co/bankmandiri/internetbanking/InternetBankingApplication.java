eeb1fd306c3255803a5a056cdc653020b250f6a1
Endy Muhardin <endy.muhardin@gmail.com>
2017-08-31 10:48:54 +0700

    implement pagination

--- internet-banking/src/main/java/id/co/bankmandiri/internetbanking/InternetBankingApplication.java
+++ internet-banking/src/main/java/id/co/bankmandiri/internetbanking/InternetBankingApplication.java
@@ -2,6 +2,8 @@ package id.co.bankmandiri.internetbanking;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.thymeleaf.dialect.springdata.SpringDataDialect;
 
 @SpringBootApplication
 public class InternetBankingApplication {
@@ -9,4 +11,9 @@ public class InternetBankingApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(InternetBankingApplication.class, args);
 	}
+	
+	@Bean
+    public SpringDataDialect springDataDialect() {
+        return new SpringDataDialect();
+    }
 }
