ba9ca22f12cc5b08a5f53e629063701c7a56a95a
Endy Muhardin <endy.muhardin@gmail.com>
2017-08-31 08:31:50 +0700

    starter project spring boot

---
+++ internet-banking/src/main/java/id/co/bankmandiri/ibank/internetbanking/InternetBankingApplication.java
@@ -0,0 +1,12 @@
+package id.co.bankmandiri.ibank.internetbanking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InternetBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InternetBankingApplication.class, args);
+	}
+}
---
+++ internet-banking/src/test/java/id/co/bankmandiri/ibank/internetbanking/InternetBankingApplicationTests.java
@@ -0,0 +1,16 @@
+package id.co.bankmandiri.ibank.internetbanking;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InternetBankingApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
